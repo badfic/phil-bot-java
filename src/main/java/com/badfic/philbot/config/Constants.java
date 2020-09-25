7ad0f2b9b086148857e2fe84b5093277499acefa
Badfic Santiago <badfic@outlook.com>
2020-09-24 20:51:38 -0700

    Allow princess of the castle to add phil/behrad/keanu phrases. closes #14

--- src/main/java/com/badfic/philbot/config/Constants.java
+++ src/main/java/com/badfic/philbot/config/Constants.java
@@ -2,5 +2,6 @@ package com.badfic.philbot.config;
 
 public interface Constants {
     String ADMIN_ROLE = "Queens of the Castle";
+    String MOD_ROLE = "Princess of the Castle";
     String EIGHTEEN_PLUS = "18+";
 }
