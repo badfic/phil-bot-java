18508b21f3bae4d91094c6f62d609f658b614e1b
Badfic Santiago <badfic@outlook.com>
2020-11-02 01:55:16 -0800

    refactor talk

--- src/main/java/com/badfic/philbot/listeners/phil/swampy/PhilTalk.java
+++ src/main/java/com/badfic/philbot/listeners/phil/swampy/PhilTalk.java
@@ -2,17 +2,11 @@ package com.badfic.philbot.listeners.phil.swampy;
 
 import com.badfic.philbot.config.PhilMarker;
 import com.badfic.philbot.listeners.BaseTalk;
-import javax.annotation.Resource;
 import net.dv8tion.jda.api.JDA;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PhilTalk extends BaseTalk implements PhilMarker {
-
-    @Resource(name = "philJda")
-    @Lazy
-    private JDA philJda;
 
     public PhilTalk() {
         super("philTalk");
