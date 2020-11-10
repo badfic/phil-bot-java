dc9f508905cfc83dbbe586ae777adaa43155f828
Badfic Santiago <badfic@outlook.com>
2020-11-09 22:01:31 -0800

    Add honeybadger error reporting

--- src/main/java/com/badfic/philbot/BotApplication.java
+++ src/main/java/com/badfic/philbot/BotApplication.java
@@ -1,5 +1,6 @@
 package com.badfic.philbot;
 
+import io.honeybadger.reporter.HoneybadgerUncaughtExceptionHandler;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ public class BotApplication {
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        HoneybadgerUncaughtExceptionHandler.registerAsUncaughtExceptionHandler();
         SpringApplication.run(BotApplication.class, args);
     }
 
