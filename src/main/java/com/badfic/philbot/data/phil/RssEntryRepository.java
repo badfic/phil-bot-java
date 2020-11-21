7565cd3553a01b805e6cf4ad0df1d2b3698826e7
Badfic Santiago <badfic@outlook.com>
2020-11-21 01:54:32 -0800

    RSS Reader functionality, closes #62

---
+++ src/main/java/com/badfic/philbot/data/phil/RssEntryRepository.java
@@ -0,0 +1,12 @@
+package com.badfic.philbot.data.phil;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RssEntryRepository extends JpaRepository<RssEntry, String>, JpaSpecificationExecutor<RssEntry> {
+
+    long countByFeedUrl(String feedUrl);
+
+}
---
+++ src/main/java/com/badfic/philbot/listeners/phil/swampy/RssSubscriber.java
@@ -0,0 +1,117 @@
+package com.badfic.philbot.listeners.phil.swampy;
+
+import com.badfic.philbot.config.Constants;
+import com.badfic.philbot.data.phil.RssEntry;
+import com.badfic.philbot.data.phil.RssEntryRepository;
+import com.google.common.collect.ImmutableSet;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import io.honeybadger.reporter.HoneybadgerReporter;
+import java.io.ByteArrayInputStream;
+import java.lang.invoke.MethodHandles;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.annotation.Resource;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.TextChannel;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RssSubscriber implements MinuteTickable {
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+    private static final int MINUTE_REFRESH_INTERVAL = 60;
+    private static final AtomicInteger REFRESH_COUNT = new AtomicInteger(-1);
+    private static final Set<String> FEEDS = ImmutableSet.of(
+            "https://archiveofourown.org/tags/39926683/feed.atom",
+            "https://archiveofourown.org/tags/41072152/feed.atom"
+    );
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private RssEntryRepository rssEntryRepository;
+
+    @Resource
+    private HoneybadgerReporter honeybadgerReporter;
+
+    @Resource(name = "philJda")
+    @Lazy
+    private JDA philJda;
+
+    @Override
+    public void tick() throws Exception {
+        if (REFRESH_COUNT.compareAndSet(-1, 0) || REFRESH_COUNT.compareAndSet(MINUTE_REFRESH_INTERVAL, 0)) {
+            EXECUTOR.submit(this::refresh);
+            return;
+        }
+
+        REFRESH_COUNT.incrementAndGet();
+    }
+
+    private void refresh() {
+        logger.info("Checking RSS feeds");
+        TextChannel sfwChannel = philJda.getTextChannelsByName("fic-recs", false).get(0);
+        TextChannel nsfwChannel = philJda.getTextChannelsByName("nsfw-fic-recs", false).get(0);
+
+        for (String url : FEEDS) {
+            try {
+                LinkedMultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+                headers.add(HttpHeaders.ACCEPT, "application/atom+xml");
+                headers.add(HttpHeaders.USER_AGENT, "swamp");
+                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+                SyndFeed feed = new SyndFeedInput().build(new XmlReader(new ByteArrayInputStream(response.getBody().getBytes())));
+
+                boolean initialLoad = rssEntryRepository.countByFeedUrl(url) == 0;
+                long addedLinks = 0;
+
+                for (SyndEntry entry : feed.getEntries()) {
+                    String link = entry.getLink();
+
+                    if (!rssEntryRepository.existsById(link)) {
+                        rssEntryRepository.save(new RssEntry(link, url));
+                        addedLinks++;
+
+                        if (!initialLoad) {
+                            // todo parse link, extract metadata
+                            if (StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "teen and up audience")
+                                    || StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "general audience")) {
+                                sfwChannel.sendMessage("\uD83D\uDCF0\n" + link).queue();
+                            } else if (StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "mature")
+                                    || StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "explicit")
+                                    || StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "not rated")) {
+                                nsfwChannel.sendMessage("\uD83D\uDCF0\n" + link).queue();
+                            }
+                        }
+                    }
+                }
+
+                if (initialLoad) {
+                    philJda.getTextChannelsByName(Constants.TEST_CHANNEL, false)
+                            .get(0)
+                            .sendMessage("Successfully loaded " + addedLinks + " rss entries into db from " + url)
+                            .queue();
+                }
+            } catch (Exception e) {
+                logger.error("Failed to parse rss feed {}", url, e);
+                honeybadgerReporter.reportError(e, null, "Failed to parse rss feed " + url);
+            }
+        }
+        logger.info("Finished checking RSS feeds");
+    }
+}
