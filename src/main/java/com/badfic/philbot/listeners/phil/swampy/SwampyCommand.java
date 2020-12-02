4bfed381845cdb59a3a40dd800e0725eb7f91098
Badfic Santiago <badfic@outlook.com>
2020-12-01 17:33:27 -0800

    hot slots

--- src/main/java/com/badfic/philbot/listeners/phil/swampy/SwampyCommand.java
+++ src/main/java/com/badfic/philbot/listeners/phil/swampy/SwampyCommand.java
@@ -72,7 +72,7 @@ public class SwampyCommand extends BaseSwampy implements PhilMarker {
         aliases = new String[] {
                 "swamp", "bastard", "spooky", "cursed", "gay", "aww", "moist", "moisten", "gobble", "sin", "simp", "simpy", "shrimp", "shrimpy", "shremp",
                 "daddy", "punny", "dakota", "grapefruit", "stregg", "destiel", "foot", "oleo", "shack", "pit", "jolly", "shrantiago", "shrek", "billy",
-                "deer"};
+                "deer", "hot"};
         help =
                 "`!!swampy` aka... `" + Arrays.toString(aliases) + "` HELP:\n" +
                 "`!!swampy rank` show your swampy rank\n" +
