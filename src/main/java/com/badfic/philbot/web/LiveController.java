bb1349bdac604a71d88c3fb808cafde9bb09155b
Badfic Santiago <badfic@outlook.com>
2020-12-28 00:50:06 -0800

    initial test of live page

---
+++ src/main/java/com/badfic/philbot/web/LiveController.java
@@ -0,0 +1,36 @@
+package com.badfic.philbot.web;
+
+import com.github.mustachejava.Mustache;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LiveController extends BaseController {
+
+    private Mustache mustache;
+
+    @PostConstruct
+    public void init() {
+        mustache = mustacheFactory.compile("live.mustache");
+    }
+
+    @GetMapping(value = "/live", produces = MediaType.TEXT_HTML_VALUE)
+    public ResponseEntity<String> get(HttpSession httpSession) throws Exception {
+        checkSession(httpSession, false);
+
+        Map<String, Object> props = new HashMap<>();
+        props.put("pageTitle", "The Swamp Live");
+        props.put("username", httpSession.getAttribute(DISCORD_USERNAME));
+
+        try (ReusableStringWriter stringWriter = ReusableStringWriter.getCurrent()) {
+            mustache.execute(stringWriter, props);
+            return ResponseEntity.ok(stringWriter.toString());
+        }
+    }
+}
