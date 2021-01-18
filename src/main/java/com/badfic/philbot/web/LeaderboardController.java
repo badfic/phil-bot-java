234346b70cb5b5cff7b74d661374ccafe74972ff
Badfic Santiago <badfic@outlook.com>
2021-01-17 22:40:55 -0800

    leaderboard contains statistics now

--- src/main/java/com/badfic/philbot/web/LeaderboardController.java
+++ src/main/java/com/badfic/philbot/web/LeaderboardController.java
@@ -4,7 +4,7 @@ import com.badfic.philbot.config.Constants;
 import com.badfic.philbot.data.DiscordUser;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
-import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,9 +16,11 @@ import net.dv8tion.jda.api.entities.Member;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/leaderboard")
 public class LeaderboardController extends BaseController {
 
     @Resource
@@ -31,80 +33,56 @@ public class LeaderboardController extends BaseController {
         mustache = mustacheFactory.compile("leaderboard.mustache");
     }
 
-    @GetMapping(value = "/leaderboard", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
     public ResponseEntity<String> getRanks(HttpSession httpSession) throws Exception {
         checkSession(httpSession, false);
-
-        return ResponseEntity.ok(leaderboard(httpSession));
-    }
-
-    private String leaderboard(HttpSession httpSession) {
-        List<DiscordUser> swampyUsers = discordUserRepository.findAll();
-
-        List<SimpleMember> bastards = swampyUsers.stream()
-                .sorted((u1, u2) -> Long.compare(u2.getXp(), u1.getXp()))
-                .filter(user -> {
-                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
-                    return member != null && hasRole(member, Constants.EIGHTEEN_PLUS_ROLE);
-                })
-                .map(user -> {
-                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
-                    return new SimpleMember(member.getUser().getEffectiveAvatarUrl(), member.getEffectiveName(), member.getId(),
-                            NumberFormat.getIntegerInstance().format(user.getXp()));
-                })
-                .collect(Collectors.toList());
-
-        List<SimpleMember> children = swampyUsers.stream()
-                .sorted((u1, u2) -> Long.compare(u2.getXp(), u1.getXp()))
-                .filter(user -> {
-                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
-                    return member != null && hasRole(member, Constants.CHAOS_CHILDREN_ROLE);
-                })
-                .map(user -> {
-                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
-                    return new SimpleMember(member.getUser().getEffectiveAvatarUrl(), member.getEffectiveName(), member.getId(),
-                            NumberFormat.getIntegerInstance().format(user.getXp()));
-                })
-                .collect(Collectors.toList());
 
         Map<String, Object> props = new HashMap<>();
         props.put("pageTitle", "Leaderboard");
         props.put("username", httpSession.getAttribute(DISCORD_USERNAME));
-        props.put("bastards", bastards);
-        props.put("children", children);
+        props.put("tables", Arrays.asList("bastards", "children"));
         try (ReusableStringWriter stringWriter = ReusableStringWriter.getCurrent()) {
             mustache.execute(stringWriter, props);
-            return stringWriter.toString();
+            return ResponseEntity.ok(stringWriter.toString());
         }
     }
 
-    public static class SimpleMember {
-        private final String image;
-        private final String name;
-        private final String id;
-        private final String xp;
+    @GetMapping(value = "/bastards", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<DiscordUser> getBastards(HttpSession httpSession) throws Exception {
+        checkSession(httpSession, false);
 
-        public SimpleMember(String image, String name, String id, String xp) {
-            this.image = image;
-            this.name = name;
-            this.id = id;
-            this.xp = xp;
-        }
+        List<DiscordUser> swampyUsers = discordUserRepository.findAll();
 
-        public String getImage() {
-            return image;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getXp() {
-            return xp;
-        }
+        return swampyUsers.stream()
+                .filter(user -> {
+                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
+                    return member != null && hasRole(member, Constants.EIGHTEEN_PLUS_ROLE);
+                })
+                .peek(user -> {
+                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
+                    user.setNickname(member.getEffectiveName());
+                    user.setProfileUrl(member.getUser().getEffectiveAvatarUrl());
+                })
+                .collect(Collectors.toList());
     }
+
+    @GetMapping(value = "/children", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<DiscordUser> getChildren(HttpSession httpSession) throws Exception {
+        checkSession(httpSession, false);
+
+        List<DiscordUser> swampyUsers = discordUserRepository.findAll();
+
+        return swampyUsers.stream()
+                .filter(user -> {
+                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
+                    return member != null && hasRole(member, Constants.CHAOS_CHILDREN_ROLE);
+                })
+                .peek(user -> {
+                    Member member = philJda.getGuilds().get(0).getMemberById(user.getId());
+                    user.setNickname(member.getEffectiveName());
+                    user.setProfileUrl(member.getUser().getEffectiveAvatarUrl());
+                })
+                .collect(Collectors.toList());
+    }
+
 }
