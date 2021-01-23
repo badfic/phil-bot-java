1e7b9f1b1a0e41220565c462c6c6fbc634c8f3ff
Badfic Santiago <badfic@outlook.com>
2021-01-23 00:48:20 -0800

    cheap pfp command

---
+++ src/main/java/com/badfic/philbot/listeners/phil/swampy/PfpCommand.java
@@ -0,0 +1,31 @@
+package com.badfic.philbot.listeners.phil.swampy;
+
+import com.badfic.philbot.config.Constants;
+import com.badfic.philbot.config.PhilMarker;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import java.util.List;
+import net.dv8tion.jda.api.entities.Member;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PfpCommand extends BaseSwampy implements PhilMarker {
+
+    public PfpCommand() {
+        name = "pfp";
+        help = "`!!pfp @user`\n" +
+                "show that user's profile picture, large";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        Member member = event.getMember();
+        List<Member> mentionedMembers = event.getMessage().getMentionedMembers();
+        if (CollectionUtils.isNotEmpty(mentionedMembers)) {
+            member = mentionedMembers.get(0);
+        }
+
+        event.reply(Constants.simpleEmbed(member.getEffectiveName(), null, member.getUser().getEffectiveAvatarUrl()));
+    }
+
+}
