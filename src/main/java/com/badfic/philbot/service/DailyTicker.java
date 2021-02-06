96858e05391fecdd7dfa48f1e5667532d539c99b
Badfic Santiago <badfic@outlook.com>
2021-02-06 10:32:58 -0800

    daily riverdale meme cache

--- src/main/java/com/badfic/philbot/service/DailyTicker.java
+++ src/main/java/com/badfic/philbot/service/DailyTicker.java
@@ -18,12 +18,14 @@ public class DailyTicker extends BaseService {
     @Scheduled(cron = "0 0 12 * * ?", zone = "GMT")
     public void masterTick() {
         for (DailyTickable runnable : dailyTickables) {
-            try {
-                runnable.run();
-            } catch (Exception e) {
-                logger.error("Exception in daily tickable [{}]", runnable.getClass().getName(), e);
-                honeybadgerReporter.reportError(e, null, "Exception in daily tickable: " + runnable.getClass().getName());
-            }
+            threadPoolTaskExecutor.submit(() -> {
+                try {
+                    runnable.run();
+                } catch (Exception e) {
+                    logger.error("Exception in daily tickable [{}]", runnable.getClass().getName(), e);
+                    honeybadgerReporter.reportError(e, null, "Exception in daily tickable: " + runnable.getClass().getName());
+                }
+            });
         }
     }
 
