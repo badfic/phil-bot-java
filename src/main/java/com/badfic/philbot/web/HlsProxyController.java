50e2e77f671b0e538c8816c5e043a442d7f1f773
Badfic Santiago <badfic@outlook.com>
2021-05-10 01:49:30 -0700

    try to fix auth to hls endpoints

--- src/main/java/com/badfic/philbot/web/HlsProxyController.java
+++ src/main/java/com/badfic/philbot/web/HlsProxyController.java
@@ -6,13 +6,12 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,20 +21,19 @@ public class HlsProxyController extends BaseController {
             .expireAfterWrite(Duration.of(15, ChronoUnit.MINUTES))
             .build();
 
-    @GetMapping(value = "/hls/**")
-    public ResponseEntity<byte[]> hlsProxy(HttpServletRequest httpServletRequest, @RequestParam("auth") Optional<String> auth) {
+    @GetMapping(value = "/{authCode}/hls/**")
+    public ResponseEntity<byte[]> hlsProxy(HttpServletRequest httpServletRequest, @PathVariable("authCode") String auth) {
         HttpSession httpSession = httpServletRequest.getSession();
 
-        if (auth.isPresent()) {
-            if (OUTSTANDING_AUTHS.getIfPresent(auth.get()) != null) {
-                httpSession.setAttribute(CHROMECAST_AUTH, auth.get());
-            }
+        if (httpSession.getAttribute(CHROMECAST_AUTH) == null && OUTSTANDING_AUTHS.getIfPresent(auth) != null) {
+            httpSession.setAttribute(CHROMECAST_AUTH, auth);
         }
         if (httpSession.getAttribute(CHROMECAST_AUTH) == null) {
             throw new UnauthorizedException("You do not have a valid session. Please refresh and login again");
         }
 
-        return restTemplate.getForEntity("http://" + baseConfig.owncastInstance + httpServletRequest.getRequestURI(), byte[].class);
+        String targetUrl = "http://" + baseConfig.owncastInstance + httpServletRequest.getRequestURI().replace("/" + auth, "");
+        return restTemplate.getForEntity(targetUrl, byte[].class);
     }
 
     @GetMapping(value = "chromecast-auth")
