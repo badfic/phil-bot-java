3bc41ff36cce4c4cb303a4eb6fc8962fdb254025
Badfic Santiago <badfic@outlook.com>
2021-05-09 23:26:58 -0700

    disable auth on hls endpoints

--- src/main/java/com/badfic/philbot/web/HlsProxyController.java
+++ src/main/java/com/badfic/philbot/web/HlsProxyController.java
@@ -1,9 +1,6 @@
 package com.badfic.philbot.web;
 
-import com.badfic.philbot.config.NewSessionException;
-import com.badfic.philbot.config.UnauthorizedException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +10,13 @@ public class HlsProxyController extends BaseController {
 
     @GetMapping(value = "/hls/**")
     public ResponseEntity<byte[]> hlsProxy(HttpServletRequest httpServletRequest) {
-        HttpSession httpSession = httpServletRequest.getSession();
-        if (httpSession.isNew()) {
-            throw new NewSessionException();
-        }
-        if (httpSession.getAttribute(DISCORD_TOKEN) == null) {
-            throw new UnauthorizedException("You do not have a valid session. Please refresh and login again");
-        }
+//        HttpSession httpSession = httpServletRequest.getSession();
+//        if (httpSession.isNew()) {
+//            throw new NewSessionException();
+//        }
+//        if (httpSession.getAttribute(DISCORD_TOKEN) == null) {
+//            throw new UnauthorizedException("You do not have a valid session. Please refresh and login again");
+//        }
 
         return restTemplate.getForEntity("http://" + baseConfig.owncastInstance + httpServletRequest.getRequestURI(), byte[].class);
     }
