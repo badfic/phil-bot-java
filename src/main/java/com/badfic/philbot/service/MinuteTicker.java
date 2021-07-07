83f1040dc9dfc1f62964e5cffb57639a781ae9e1
Badfic Santiago <badfic@outlook.com>
2021-07-06 21:36:34 -0700

    add philJda safety to minute ticker upon startup

--- src/main/java/com/badfic/philbot/service/MinuteTicker.java
+++ src/main/java/com/badfic/philbot/service/MinuteTicker.java
@@ -3,6 +3,7 @@ package com.badfic.philbot.service;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
 import javax.annotation.Resource;
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,6 +18,11 @@ public class MinuteTicker extends BaseService {
 
     @Scheduled(cron = "0 * * * * ?", zone = "GMT")
     public void masterTick() {
+        if (philJda == null || CollectionUtils.isEmpty(philJda.getGuilds())) {
+            logger.info("Still setting up philJDA, MinuteTicker shows it as null");
+            return; // Context is still setting up and the minute rolled over before phil received the ready message from discord api.
+        }
+
         for (MinuteTickable runnable : minuteTickables) {
             try {
                 runnable.run();
