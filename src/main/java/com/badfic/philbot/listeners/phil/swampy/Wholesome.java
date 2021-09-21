bf71fff848254bea8f3344a4f4186e2589d12508
Badfic Santiago <badfic@outlook.com>
2021-09-21 00:01:56 -0700

    add a new wholesome image

--- src/main/java/com/badfic/philbot/listeners/phil/swampy/Wholesome.java
+++ src/main/java/com/badfic/philbot/listeners/phil/swampy/Wholesome.java
@@ -44,6 +44,7 @@ public class Wholesome extends BaseSwampy {
             .add("https://cdn.discordapp.com/attachments/323666308107599872/876373213184131072/unknown.png")
             .add("https://cdn.discordapp.com/attachments/810711700818100244/885280804970848276/0e3c7fddbe517e7cb35e2e7e7109057bec27f2a2.png")
             .add("https://cdn.discordapp.com/attachments/810711700818100244/885280298382802954/df6b6401681304ad7eeb6ce2b18d6fb3.png")
+            .add("https://cdn.discordapp.com/attachments/741000350500126753/887827931261456465/image0.jpg")
             .build();
 
     public Wholesome() {
