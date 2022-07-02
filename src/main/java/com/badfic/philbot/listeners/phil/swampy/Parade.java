faba0e49640331964f96413d7188ad2d81b21129
Badfic Santiago <badfic@outlook.com>
2022-07-02 15:24:16 -0700

    disable parade schedule

--- src/main/java/com/badfic/philbot/listeners/phil/swampy/Parade.java
+++ src/main/java/com/badfic/philbot/listeners/phil/swampy/Parade.java
@@ -4,13 +4,10 @@ import com.badfic.philbot.config.Constants;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -28,15 +25,14 @@ public class Parade extends BaseSwampy {
 
     @PostConstruct
     public void init() throws Exception {
-        List<ParadeItem> paradeItemsArray =
-                Arrays.asList(objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("parade.json"), ParadeItem[].class));
+        ParadeItem[] paradeItemsArray = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("parade.json"), ParadeItem[].class);
 
         for (ParadeItem paradeItem : paradeItemsArray) {
             PARADE_ITEMS.put(paradeItem.day, paradeItem);
         }
     }
 
-    @Scheduled(cron = "${swampy.schedule.events.parade}", zone = "${swampy.schedule.timezone}")
+    // @Scheduled(cron = "${swampy.schedule.events.parade}", zone = "${swampy.schedule.timezone}")
     public void runParade() {
         LocalDate today = LocalDate.now(ZoneId.of(swampyTimezone));
         int dayOfMonth = today.getDayOfMonth();
