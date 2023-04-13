ae6f8506cc0147cfdddd5369bf228871423d343e
Badfic Santiago <badfic@outlook.com>
2023-04-13 01:36:52 -0700

    Remove jumblr library, make manual api call to tumblr

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM quay.io/badfic/liberica-runtime:jre-17-musl
 COPY target/philbot-0.0.1-SNAPSHOT.jar ./philbot.jar
 EXPOSE 8080
-ENTRYPOINT java -Xms64m -Xmx256m -XX:+UseSerialGC -verbose:gc -Dspring.backgroundpreinitializer.ignore=true -Dspring.config.location=classpath:application.yaml --add-opens java.base/java.io=ALL-UNNAMED -jar philbot.jar
+ENTRYPOINT java -Xms64m -Xmx256m -XX:+UseSerialGC -verbose:gc -Dspring.backgroundpreinitializer.ignore=true -Dspring.config.location=classpath:application.yaml -jar philbot.jar
--- src/main/java/com/badfic/philbot/commands/Tumblr.java
+++ src/main/java/com/badfic/philbot/commands/Tumblr.java
@@ -1,24 +1,29 @@
 package com.badfic.philbot.commands;
 
+import com.badfic.philbot.config.BaseConfig;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jagrosh.jdautilities.command.CommandEvent;
-import com.tumblr.jumblr.JumblrClient;
-import com.tumblr.jumblr.types.Blog;
-import com.tumblr.jumblr.types.Post;
+import java.time.Instant;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-@Component
 @Slf4j
+@Component
 public class Tumblr extends BaseNormalCommand {
-    private final JumblrClient jumblrClient;
+    private final RestTemplate restTemplate;
+    private final String apiKey;
 
-    public Tumblr(JumblrClient jumblrClient) {
+    public Tumblr(RestTemplate restTemplate, BaseConfig baseConfig) {
         name = "tumblr";
         help = "!!tumblr username\nShow this tumblr users latest post";
-        this.jumblrClient = jumblrClient;
+        this.restTemplate = restTemplate;
+        this.apiKey = baseConfig.tumblrConsumerKey;
     }
 
     @Override
@@ -32,16 +37,21 @@ public class Tumblr extends BaseNormalCommand {
 
         String tumblrUser = split[0];
         try {
-            Blog blog = jumblrClient.blogInfo(tumblrUser + ".tumblr.com");
+            ResponseEntity<Info> responseEntity = restTemplate.getForEntity(
+                    String.format("https://api.tumblr.com/v2/blog/%s/posts?api_key=%s", tumblrUser, apiKey), Info.class);
 
-            if (blog != null) {
-                List<Post> posts = blog.posts();
-                posts.sort((a, b) -> Long.compare(b.getTimestamp(), a.getTimestamp()));
+            Info info = responseEntity.getBody();
+
+            if (info != null) {
+                Response response = info.response();
+                List<Post> posts = response.posts();
 
                 if (CollectionUtils.isNotEmpty(posts)) {
+                    posts.sort((a, b) -> Long.compare(b.timestamp().getEpochSecond(), a.timestamp().getEpochSecond()));
+
                     Post post = posts.get(0);
 
-                    event.reply("\uD83D\uDD16 **" + blog.getTitle() + "** \n\uD83D\uDD8A️ **Last Post**: " + post.getPostUrl());
+                    event.reply("\uD83D\uDD16 **" + response.blog().title() + "** \n\uD83D\uDD8A️ **Last Post**: " + post.postUrl());
                 }
             }
         } catch (Exception e) {
@@ -49,5 +59,17 @@ public class Tumblr extends BaseNormalCommand {
             event.replyError("Something went wrong trying to get user's posts: " + tumblrUser);
         }
     }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private record Info(Response response) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private record Response(Blog blog, List<Post> posts) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private record Blog(String title) {}
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private record Post(Instant timestamp, @JsonProperty("post_url") String postUrl) {}
 
 }
