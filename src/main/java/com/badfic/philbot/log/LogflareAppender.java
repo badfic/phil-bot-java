903352730ddd171e6f4a43b721ca640374c3fec4
Badfic Santiago <badfic@outlook.com>
2023-04-21 02:57:10 -0700

    update README, simplify LogflareAppender

--- src/main/java/com/badfic/philbot/log/LogflareAppender.java
+++ src/main/java/com/badfic/philbot/log/LogflareAppender.java
@@ -3,61 +3,41 @@ package com.badfic.philbot.log;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 
-@Component
-@NoArgsConstructor
-@Getter
-@Setter
 public class LogflareAppender extends AppenderBase<ILoggingEvent> {
 
-    // Set by Spring Autowiring
-    private static RestTemplate REST_TEMPLATE;
-    private static ObjectMapper OBJECT_MAPPER;
+    // Set by logback config properties
+    @Getter @Setter private PatternLayoutEncoder encoder;
+    @Getter @Setter private String logflareUrl;
+    @Getter @Setter private String logflareApiKey;
 
-    // Set by config properties
-    private PatternLayoutEncoder encoder;
-    private String logflareUrl;
-    private String logflareApiKey;
-
-    // Set on start
-    private BlockingQueue<String> queue;
-    private Thread worker;
+    private final RestTemplate restTemplate;
+    private final BlockingQueue<String> queue;
+    private final Thread worker;
     private volatile boolean workerStopped;
 
-    @Autowired
-    public LogflareAppender(RestTemplate restTemplate, ObjectMapper objectMapper) {
-        REST_TEMPLATE = restTemplate;
-        OBJECT_MAPPER = objectMapper;
-    }
-
-    @Override
-    public void start() {
+    public LogflareAppender() {
+        restTemplate = new RestTemplate();
         queue = new ArrayBlockingQueue<>(256);
         worker = new Thread(() -> {
             while (!workerStopped && !Thread.currentThread().isInterrupted()) {
                 try {
-                    if (REST_TEMPLATE != null) {
-                        String message = queue.poll();
+                    String message = queue.poll();
 
-                        if (message != null) {
-                            sendMessage(message);
-                        }
+                    if (message != null) {
+                        sendMessage(message);
                     }
 
                     // The rate limit for Logflare is 5 per second
@@ -65,6 +45,10 @@ public class LogflareAppender extends AppenderBase<ILoggingEvent> {
                 } catch (Exception ignored) {}
             }
         });
+    }
+
+    @Override
+    public void start() {
         worker.start();
         super.start();
     }
@@ -97,12 +81,10 @@ public class LogflareAppender extends AppenderBase<ILoggingEvent> {
     }
 
     private void sendMessage(String message) {
-        REST_TEMPLATE.execute(logflareUrl, HttpMethod.POST,
-                request -> {
-                    request.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-                    request.getHeaders().set("X-API-KEY", logflareApiKey);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        headers.set("X-API-KEY", logflareApiKey);
 
-                    request.getBody().write(OBJECT_MAPPER.writeValueAsBytes(Map.of("message", message)));
-                }, (ResponseExtractor<String>) response -> null);
+        restTemplate.exchange(logflareUrl, HttpMethod.POST, new HttpEntity<>(Map.of("message", message), headers), String.class);
     }
 }
