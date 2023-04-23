f416c0935cafb08dd33f494716643143c6f8ccfd
Santiago <badfic@outlook.com>
2023-04-22 21:57:18 -0700

    HungerGames responds to reaction (closes #150) (#159)

--- src/main/java/com/badfic/philbot/service/HungerGamesWinnersService.java
+++ src/main/java/com/badfic/philbot/service/HungerGamesWinnersService.java
@@ -5,23 +5,18 @@ import com.badfic.philbot.config.Constants;
 import com.badfic.philbot.data.HungerGamesWinnerEntity;
 import com.badfic.philbot.data.HungerGamesWinnerRepository;
 import com.jagrosh.jdautilities.command.CommandEvent;
-import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.MessageHistory;
-import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.emoji.CustomEmoji;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -30,7 +25,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class HungerGamesWinnersService extends BaseNormalCommand implements DailyTickable {
+public class HungerGamesWinnersService extends BaseNormalCommand {
 
     private static final String REACTION_EMOJI = "\uD83C\uDFC1";
 
@@ -38,84 +33,73 @@ public class HungerGamesWinnersService extends BaseNormalCommand implements Dail
     private final JdbcAggregateTemplate jdbcAggregateTemplate;
 
     public HungerGamesWinnersService(HungerGamesWinnerRepository hungerGamesWinnerRepository, JdbcAggregateTemplate jdbcAggregateTemplate) {
-        name = "updateHungerGamesWinners";
-        ownerCommand = true;
+        name = "hungerGames";
+        requiredRole = Constants.ADMIN_ROLE;
+
         this.hungerGamesWinnerRepository = hungerGamesWinnerRepository;
         this.jdbcAggregateTemplate = jdbcAggregateTemplate;
     }
 
     @Override
     protected void execute(CommandEvent event) {
-        threadPoolTaskExecutor.execute(this::runDailyTask);
-    }
+        String[] args = event.getArgs().split("\\s+");
 
-    @Override
-    public void runDailyTask() {
-        Optional<TextChannel> optionalChannel = philJda.getTextChannelsByName(Constants.HUNGERDOME_CHANNEL, false)
-                .stream()
-                .findFirst();
+        if ("delete".equalsIgnoreCase(args[0])) {
+            long messageId;
+            try {
+                messageId = Long.parseLong(args[1]);
+            } catch (NumberFormatException nfe) {
+                event.replyError("Could not parse messageId: " + args[1]);
+                return;
+            }
 
-        if (optionalChannel.isEmpty()) {
-            log.error("HungerGamesWinnersService Could not find [channel={}]", Constants.HUNGERDOME_CHANNEL);
+            hungerGamesWinnerRepository.deleteById(messageId);
+            event.replySuccess("Deleted hunger games message: " + messageId);
             return;
         }
 
-        TextChannel channel = optionalChannel.get();
-        Set<Long> messageIds = new HashSet<>();
+        event.replyError("Could not parse arg: " + args[0]);
+    }
 
-        long lastMsgId = 0;
-        while (lastMsgId != -1) {
-            MessageHistory history;
-            if (lastMsgId == 0) {
-                history = channel.getHistoryFromBeginning(100).timeout(30, TimeUnit.SECONDS).complete();
-            } else {
-                history = channel.getHistoryAfter(lastMsgId, 100).timeout(30, TimeUnit.SECONDS).complete();
-            }
+    public void addWinner(MessageReactionAddEvent event) {
+        long messageId = event.getMessageIdLong();
+        long channelId = event.getChannel().getIdLong();
 
-            lastMsgId = CollectionUtils.isNotEmpty(history.getRetrievedHistory())
-                    ? history.getRetrievedHistory().get(0).getIdLong()
-                    : -1;
+        TextChannel textChannel = philJda.getTextChannelById(channelId);
 
-            for (Message message : history.getRetrievedHistory()) {
-                MessageReaction reaction = message.getReaction(Emoji.fromUnicode(REACTION_EMOJI));
+        if (!"the-hungerdome".equals(textChannel.getName())) {
+            return;
+        }
 
-                if (reaction != null) {
-                    messageIds.add(message.getIdLong());
+        textChannel.retrieveMessageById(messageId).queue(message -> {
+            HungerGamesWinnerEntity storedMessage;
+            Optional<HungerGamesWinnerEntity> storedMessageOpt = hungerGamesWinnerRepository.findById(messageId);
 
-                    HungerGamesWinnerEntity storedMessage;
-                    Optional<HungerGamesWinnerEntity> storedMessageOpt = hungerGamesWinnerRepository.findById(message.getIdLong());
+            if (storedMessageOpt.isPresent()) {
+                storedMessage = storedMessageOpt.get();
 
-                    if (storedMessageOpt.isPresent()) {
-                        storedMessage = storedMessageOpt.get();
+                if (Objects.nonNull(message.getTimeEdited()) &&
+                        !message.getTimeEdited().toLocalDateTime().isEqual(storedMessage.getTimeEdited())) {
+                    String content = getMessageContent(message);
 
-                        if (Objects.nonNull(message.getTimeEdited()) &&
-                                !message.getTimeEdited().toLocalDateTime().isEqual(storedMessage.getTimeEdited())) {
-                            String content = getMessageContent(message);
-
-                            storedMessage.setMessage(content);
-                            storedMessage.setTimeEdited(message.getTimeEdited().toLocalDateTime());
-                            hungerGamesWinnerRepository.save(storedMessage);
-                        }
-                    } else {
-                        String content = getMessageContent(message);
-
-                        storedMessage = new HungerGamesWinnerEntity(message.getIdLong(), content, message.getTimeCreated().toLocalDateTime(),
-                                message.getTimeEdited() != null ? message.getTimeEdited().toLocalDateTime() : message.getTimeCreated().toLocalDateTime());
-                        jdbcAggregateTemplate.insert(storedMessage);
-                    }
+                    storedMessage.setMessage(content);
+                    storedMessage.setTimeEdited(message.getTimeEdited().toLocalDateTime());
+                    hungerGamesWinnerRepository.save(storedMessage);
                 }
+            } else {
+                String content = getMessageContent(message);
+
+                storedMessage = new HungerGamesWinnerEntity(message.getIdLong(), content, message.getTimeCreated().toLocalDateTime(),
+                        message.getTimeEdited() != null ? message.getTimeEdited().toLocalDateTime() : message.getTimeCreated().toLocalDateTime());
+                jdbcAggregateTemplate.insert(storedMessage);
             }
-        }
 
-        Collection<Long> deletedMessageIds = CollectionUtils.disjunction(hungerGamesWinnerRepository.findAllIds(), messageIds);
+            message.addReaction(Emoji.fromUnicode("✅")).queue();
+        });
+    }
 
-        for (Long deletedMessageId : deletedMessageIds) {
-            if (hungerGamesWinnerRepository.existsById(deletedMessageId)) {
-                hungerGamesWinnerRepository.deleteById(deletedMessageId);
-            }
-        }
-
-        Constants.debugToTestChannel(philJda, "Successfully updated " + Constants.HUNGERDOME_CHANNEL + " cache");
+    public String getEmoji() {
+        return REACTION_EMOJI;
     }
 
     public List<String> getMessages() {
