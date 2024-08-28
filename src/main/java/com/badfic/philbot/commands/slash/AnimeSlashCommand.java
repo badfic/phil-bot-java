bc55c831c1b7f59e92db2049595dabf3b7038cfc
Santiago <badfic@outlook.com>
2024-08-27 23:06:03 -0700

    fix anime command autocomplete (#236)

--- src/main/java/com/badfic/philbot/commands/slash/AnimeSlashCommand.java
+++ src/main/java/com/badfic/philbot/commands/slash/AnimeSlashCommand.java
@@ -1,5 +1,8 @@
 package com.badfic.philbot.commands.slash;
 
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMaps;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -10,6 +13,8 @@ import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.utils.FileUpload;
+import org.apache.commons.lang3.EnumUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,14 +25,19 @@ class AnimeSlashCommand extends BaseSlashCommand {
         peck, poke, pout, punch, shoot, shrug, slap, sleep, smile, smug, stare, think, thumbsup, wave, wink, yawn, yeet
     }
 
+    private final Long2ObjectMap<Command.Choice[]> userCategoryCache;
+
     AnimeSlashCommand() {
         name = "anime";
         options = List.of(new OptionData(OptionType.STRING, "category", "Category", true, true));
         help = "Show anime gif for the given category";
+        userCategoryCache = Long2ObjectMaps.synchronize(new Long2ObjectOpenHashMap<>());
     }
 
     @Override
     public void execute(final SlashCommandInteractionEvent event) {
+        userCategoryCache.remove(event.getUser().getIdLong());
+
         final var interactionHook = event.deferReply().submit();
         final var categoryString = event.getOption("category");
 
@@ -36,21 +46,32 @@ class AnimeSlashCommand extends BaseSlashCommand {
             return;
         }
 
-        final var category = Category.values()[categoryString.getAsInt()];
+        final var category = EnumUtils.getEnumIgnoreCase(Category.class, categoryString.getAsString(), Category.nope);
 
-        final var result = restTemplate.getForObject("https://nekos.best/api/v2/" + category.name(), AnimeResultList.class).results()[0];
-        final var imageBytes = restTemplate.getForObject(result.url(), byte[].class);
-
-        replyToInteractionHook(event, interactionHook, FileUpload.fromData(imageBytes, "anime.gif"));
+        try {
+            final var result = restTemplate.getForObject("https://nekos.best/api/v2/" + category.name(), AnimeResultList.class).results()[0];
+            final var imageBytes = restTemplate.getForObject(result.url(), byte[].class);
+            replyToInteractionHook(event, interactionHook, FileUpload.fromData(imageBytes, "anime.gif"));
+        } catch (final Exception e) {
+            log.error("Failed to find anime gif for [category={}]", category, e);
+            replyToInteractionHook(event, interactionHook, "Could not find anime gif \uD83D\uDE14");
+        }
     }
 
     @Override
-    public void onAutoComplete(CommandAutoCompleteInteractionEvent event) {
+    public void onAutoComplete(final CommandAutoCompleteInteractionEvent event) {
+        final var userId = event.getUser().getIdLong();
+        final var choices = userCategoryCache.computeIfAbsent(userId, m -> {
+            return Arrays.stream(Category.values())
+                    .map(c -> new Command.Choice(c.name(), c.name()))
+                    .sorted((a, b) -> randomNumberService.nextInt(-1, 2))
+                    .limit(25)
+                    .toArray(Command.Choice[]::new);
+        });
+
         event.replyChoices(
-                Arrays.stream(Category.values())
-                .map(c -> new Command.Choice(c.name(), c.ordinal()))
-                .sorted((o1, o2) -> randomNumberService.nextInt(-1, 2))
-                .limit(25)
+                Arrays.stream(choices)
+                .filter(a -> StringUtils.startsWithIgnoreCase(a.getName(), event.getOption("category").getAsString()))
                 .toArray(Command.Choice[]::new)
         ).queue();
     }
