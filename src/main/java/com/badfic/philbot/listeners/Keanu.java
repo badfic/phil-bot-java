8378b6dc967b6c3b5a33061e90f82ac1e74a988b
Santiago <badfic@outlook.com>
2024-09-26 00:43:30 -0700

    remove keanu morning gifs (#241)

--- src/main/java/com/badfic/philbot/listeners/Keanu.java
+++ src/main/java/com/badfic/philbot/listeners/Keanu.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 class Keanu {
@@ -54,46 +53,11 @@ class Keanu {
                 "https://cdn.discordapp.com/attachments/794506942906761226/1095950269977743380/SlimCarelessDalmatian.gif",
                 "https://cdn.discordapp.com/attachments/794506942906761226/1095950252047093780/EverlastingHastyHorseshoebat.gif"
         };
-        private static final String[] GOOD_MORNING_GIFS = {
-                "https://media.giphy.com/media/8rFNes6jllJQRnHTsF/giphy.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638622099832852/donut.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638643562086432/smilesmile.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638684641230908/hecute.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638695999537192/beardrub.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638707026362368/bow.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638709043691580/keanu.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638732288393256/yeah.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638754799353887/eyy.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638757303484526/eyebrows.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638770238586950/kitteneanu.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638772667220049/gay.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638780715958403/imweird_imaweirdo.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638791629668493/keanu_by_firelight.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638807534469141/keanu_smooch.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638810201915392/keanu_stark.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638826379346031/breathtaking.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638835518734446/breath_taking.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638836797997106/winkwonk.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638849771110410/laugh.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638864857890816/keanushark.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638874966163526/keanu_marshmallow.gif",
-                "https://cdn.discordapp.com/attachments/323666308107599872/752638879500206171/keanu_confetti.gif",
-                "https://cdn.discordapp.com/attachments/741030569307275436/753991114301898762/image0.png"
-        };
         private static final Pattern PUPPY_PATTERN = Constants.compileWords("puppy|puppies|pupper|doggo|doge");
         private static final String HELLO_GIF = "https://media.giphy.com/media/PnUatAYWMEMvmiwsyx/giphy.gif";
         private static final String PUPPIES_GIF = "https://media.giphy.com/media/8rFNes6jllJQRnHTsF/giphy.gif";
 
         private final Command keanuCommand;
-
-        @Scheduled(cron = "${swampy.schedule.keanu.goodmorning}", zone = "${swampy.schedule.timezone}")
-        void goodMorning() {
-            final var general = philJda.getTextChannelsByName("general", false).getFirst();
-            final var swampyGamesConfig = swampyGamesConfigDal.get();
-
-            discordWebhookSendService.sendMessage(general.getIdLong(), swampyGamesConfig.getKeanuNickname(), swampyGamesConfig.getKeanuAvatar(),
-                    Constants.pickRandom(GOOD_MORNING_GIFS));
-        }
 
         void onMessageReceived(final @NotNull MessageReceivedEvent event, final SwampyGamesConfig swampyGamesConfig) {
             final var msgContent = event.getMessage().getContentRaw().toLowerCase(Locale.ENGLISH);
